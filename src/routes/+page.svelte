0dbdbdc0d09dd28e955a0a730282954a8d0e40ea
AshtonFabby <ashtonfabbt@gmail.com>
2024-03-02 15:45:43 +0200

    Added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class=" text-center t">Welcome to SvelteKit</h1>
+<p class=" bg-red">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
