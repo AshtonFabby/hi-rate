17b0f4307f5dbe0293fd712c4f1dc9558764269f
grahamusai <grahamusai0@gmail.com>
2024-03-02 21:42:31 +0200

    Buyer and seller sections

---
+++ src/lib/sellSection.svelte
@@ -0,0 +1,65 @@
+<script>
+	import 'iconify-icon';
+	import Icon from '@iconify/svelte';
+	let sellerName = 'Seller';
+	let amount = '$35.00';
+</script>
+
+<h1 class="text-3xl mb-3">Want To Sell</h1>
+
+<div class="card mb-3 rounded-lg bg-white">
+	<div class="flex justify-between">
+		<div class="flex items-center p-4">
+			<div class="flex-shrink-0 bg-orange-300 p-2 rounded-full">
+				<Icon icon="fluent:money-16-regular" />
+			</div>
+			<div class="ml-4">
+				<h5 class="text-lg font-medium">{sellerName}</h5>
+				<p class="text-gray-500">Ecocash</p>
+			</div>
+		</div>
+		<div class="text-red-400 font-semibold items-center pt-8 px-4 text-right">{amount}</div>
+	</div>
+</div>
+<div class="card mb-3 rounded-lg bg-white">
+	<div class="flex justify-between">
+		<div class="flex items-center p-4">
+			<div class="flex-shrink-0 bg-orange-300 p-2 rounded-full">
+				<Icon icon="fluent:money-16-regular" />
+			</div>
+			<div class="ml-4">
+				<h5 class="text-lg font-medium">{sellerName}</h5>
+				<p class="text-gray-500">Ecocash</p>
+			</div>
+		</div>
+		<div class="text-red-400 font-semibold items-center pt-8 px-4 text-right">{amount}</div>
+	</div>
+</div>
+<div class="card mb-3 rounded-lg bg-white">
+	<div class="flex justify-between">
+		<div class="flex items-center p-4">
+			<div class="flex-shrink-0 bg-orange-300 p-2 rounded-full">
+				<Icon icon="fluent:money-16-regular" />
+			</div>
+			<div class="ml-4">
+				<h5 class="text-lg font-medium">{sellerName}</h5>
+				<p class="text-gray-500">Ecocash</p>
+			</div>
+		</div>
+		<div class="text-red-400 font-semibold items-center pt-8 px-4 text-right">{amount}</div>
+	</div>
+</div>
+<div class="card mb-3 rounded-lg bg-white">
+	<div class="flex justify-between">
+		<div class="flex items-center p-4">
+			<div class="flex-shrink-0 bg-orange-300 p-2 rounded-full">
+				<Icon icon="fluent:money-16-regular" />
+			</div>
+			<div class="ml-4">
+				<h5 class="text-lg font-medium">{sellerName}</h5>
+				<p class="text-gray-500">Ecocash</p>
+			</div>
+		</div>
+		<div class="text-red-400 font-semibold items-center pt-8 px-4 text-right">{amount}</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1 class=" text-center t">Welcome to SvelteKit</h1>
-<p class=" bg-red">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import BuySection from '$lib/buySection.svelte';
+	import SellSection from '$lib/sellSection.svelte';
+</script>
+
+<div class="flex">
+	<div class="px-10 w-1/2">
+		<BuySection />
+	</div>
+	<div class="px-10 w-1/2">
+		<SellSection />
+	</div>
+</div>
