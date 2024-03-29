e8fea7889d05452ba2a43cdeb4f308ece2a3a62f
AshtonFabby <ashtonfabbt@gmail.com>
2024-03-29 16:51:46 +0200

    added supabase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { createClient } from '@supabase/supabase-js';
+
+// @ts-ignore
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
